eac2690da0096fb28ff4afd211ba10dc28fd6723
justin@ThinkCentre-01 <driftpattern@gmail.com>
2023-05-30 08:18:03 -0700

    Fizz Buzz answered

--- lab-answers.js
+++ lab-answers.js
@@ -13,3 +13,24 @@ for (let i = 0; i <= 200; i += 2) {
   }
   
 
+////////////////////////////////
+// Fizz Buzz
+////////////////////////////////
+for (let i = 1; i <= 100; i++) {
+    let output = "";
+  
+    if (i % 3 === 0) {
+      output += "Fizz";
+    }
+  
+    if (i % 5 === 0) {
+      output += "Buzz";
+    }
+  
+    if (output === "") {
+      output = i;
+    }
+  
+    console.log(output);
+  }
+  
